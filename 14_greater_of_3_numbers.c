5cb97dae6b867a1a2a9ef9c2b87c3e4fd93ccb1d
Bhupinder0008 <54574067+Bhupinder0008@users.noreply.github.com>
2019-11-02 13:26:23 +0530

    Create 14_greater_of_3_numbers.c

---
+++ 14_greater_of_3_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the three numbers which you want to compare \n");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+printf("\n%d is the greatest number\n",a);
+else if(b>a&&b>c)
+printf("\n%d is the greatest number\n",b);
+else 
+printf("\n%d is the greatest number\n",c);
+}
+
+
+OUTPUT:
+enter the three numbers which you want to compare 
+1
+13
+8
+13 is the greatest number
