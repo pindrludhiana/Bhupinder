10cd336f5529b6b4105881857460cc6685e190d3
Bhupinder0008 <54574067+Bhupinder0008@users.noreply.github.com>
2019-11-02 12:36:16 +0530

    04_even_or_odd.c

---
+++ 04_even_or_odd.c
@@ -0,0 +1,26 @@
+
+
+#include <stdio.h>
+
+main()
+{
+int n;
+
+printf("Enter an integer\n");
+scanf("%d",&n);
+
+/*if n is completely divisible by 2 then prints even otherwise n is odd*/
+if ( n%2 == 0 )
+printf("Even\n");
+else
+printf("Odd\n");
+
+return 0;
+}
+
+Output:
+
+Enter an integer
+45
+Odd
+
