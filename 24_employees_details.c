f858be10c33bf7463ea2d199dbbf9c964a6c4ad4
Bhupinder0008 <54574067+Bhupinder0008@users.noreply.github.com>
2019-11-02 14:10:04 +0530

    Create 24_employees_details.c

---
+++ 24_employees_details.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+ 
+/*structure declaration*/
+struct employee{
+    char    name[30];
+    int     empId;
+    float   salary;
+};
+ 
+int main()
+{
+    /*declare structure variable*/
+    struct employee emp;
+     
+    /*read employee details*/
+    printf("\nEnter details :\n");
+    printf("Name ?:");          gets(emp.name);
+    printf("ID ?:");            scanf("%d",&emp.empId);
+    printf("Salary ?:");        scanf("%f",&emp.salary);
+     
+    /*print employee details*/
+    printf("\nEntered detail is:");
+    printf("Name: %s"   ,emp.name);
+    printf("Id: %d"     ,emp.empId);
+    printf("Salary: %f\n",emp.salary);
+    return 0;
+}
+
+Output:
+
+Enter details :
+Name ?:Bhupinder
+ID ?:1914125
+Salary ?:15,00000000
+
+Entered detail is:Name: BhupinderId: 1914125Salary: 15.000000
