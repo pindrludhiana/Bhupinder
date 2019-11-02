792dd7a002ae2b7a5a3578263f3952e9f5eddf0e
Bhupinder0008 <54574067+Bhupinder0008@users.noreply.github.com>
2019-11-02 13:46:28 +0530

    Create 19_transpose_of_matrix.c

---
+++ 19_transpose_of_matrix.c
@@ -0,0 +1,51 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+int m, n, c, d, matrix[10][10], transpose[10][10];
+printf("Enter the number of rows and columns of matrix \n ");
+scanf("%d%d",&m,&n); printf("Enter the elements of matrix \n");
+for( c = 0 ; c < m ; c++ )
+{
+for( d = 0 ; d < n ; d++ )
+{
+scanf("%d",&matrix[c][d]);
+}
+}
+
+for( c = 0 ; c < m ; c++ )
+{
+for( d = 0 ; d < n ; d++ )
+{
+transpose[d][c] = matrix[c][d];
+/* transpose by interchanging rows and columns */
+}
+}
+
+printf("Transpose of entered matrix :-\n");
+for( c = 0 ; c < n ; c++ )
+{
+for( d = 0 ; d < m ; d++ )
+{
+printf("%d\t",transpose[c][d]);
+}
+printf("\n");
+}
+return 0;
+}
+
+Output:
+
+Enter the number of rows and columns of matrix
+2
+3
+Enter the elements of matrix
+1 2 3
+4 5 6
+Transpose of entered matrix :-
+1  4
+2  5
+3  6
+
